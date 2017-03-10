47821f18f1514a3468a90d9917d721feb6890a39
Adriana Castaneda <adriana.lcs316@gmail.com>
2017-03-10 14:41:20 -0600

    add create erb and routes

--- profile.rb
+++ profile.rb
@@ -15,3 +15,15 @@ def delete_content(title)
   File.delete("profiles/#{title}.txt")
 end
 
+get '/new' do
+	erb :new
+end
+
+post '/create' do
+	save_content(params[:title], params[:content])
+	redirect URI.escape("/#{params[:title]}")
+end
+
+get '/:title' do
+	erb :show
+end
